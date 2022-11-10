382b37f3b24fcebea5f6698bacff9ea5e78d7b1f
PauloCesarCF <pc394108@gmail.com>
2022-11-10 14:09:25 -0300

    melhorias

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -18,23 +18,35 @@ const Home = () => {
         <section>
           <div className="containerProject">
             <div>
-              <img src={photoProjectCard} alt="foto"/>
-              <h2>Gerador de cartão</h2>
+              <a href="https://explorer-lab-card.vercel.app/">
+                <img src={photoProjectCard} alt="foto"/>
+              </a>
+              <a href="https://explorer-lab-card.vercel.app/">
+                <h2>Gerador de cartão</h2>
+              </a>
               <p>Nesse projeto o objetivo é simular um gerador de cartão na qual tem masterCard, visa e discover como opção e cada um começa com um numero inicial diferente, então por exemplo, se você colocar 4 e outros digitos vai aparecer o cartão da visa, se colocar um digito que começa com 5 e o proximo digito for entre 1 a 5 vai aparecer o cartão do mastercard e o cartão discover começa com 0611.</p>
             </div>
             <div>
-              <img src={photoProjectCardMobile} alt="foto" className="photoProjectMobile"/>
+              <a href="https://explorer-lab-card.vercel.app/">
+                <img src={photoProjectCardMobile} alt="foto" className="photoProjectMobile"/>
+              </a>
             </div>
           </div>
 
           <div className="containerProject">
             <div>
-              <img src={projectPokemons} alt="foto"/>
-              <h2>Lista Pokémon</h2>
+              <a href="https://api-pokemon-two-gold.vercel.app/">
+                <img src={projectPokemons} alt="foto"/>
+              </a>
+              <a href="https://api-pokemon-two-gold.vercel.app/">
+                <h2>Lista Pokémon</h2>
+              </a>
               <p>Nesse projeto tem uma lista de 150 pokemons, e pra cada pokemon temos um card na qual mostrar o nome do pokemon, o id e o tipo do pokemon. Tem também mua barra de pesquisar na qual você pode procurar o pokemon pelo id ou pelo nome.</p>
             </div>
             <div>
-              <img src={projectPokemonMobile} alt="foto" className="photoProjectMobile"/>
+              <a href="https://api-pokemon-two-gold.vercel.app/">
+                <img src={projectPokemonMobile} alt="foto" className="photoProjectMobile"/>
+              </a>
             </div>
           <div>
 
